ecc1561a58356791e59dc93b339e91555180df0f
Adam Dunmore <94834543+AdamDunmore@users.noreply.github.com>
2023-12-27 17:09:49 +0000

    Added Bismuth Ore to game (need to add gen)

--- src/main/java/net/fabricmc/gear/Main.java
+++ src/main/java/net/fabricmc/gear/Main.java
@@ -3,8 +3,12 @@ package net.fabricmc.gear;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.gear.items.BismuthArmor;
 import net.fabricmc.gear.items.BismuthBar;
+import net.fabricmc.gear.items.blocks.BismuthOre;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
@@ -22,6 +26,10 @@ public class Main implements ModInitializer {
 	public static final Item BISMUTH_CHESTPLATE = new ArmorItem(BISMUTH_ARMOR, EquipmentSlot.CHEST, new FabricItemSettings().group(Main.GEAR_ITEM_GROUP));
 	public static final Item BISMUTH_LEGGINGS = new ArmorItem(BISMUTH_ARMOR, EquipmentSlot.LEGS, new FabricItemSettings().group(Main.GEAR_ITEM_GROUP));
 	public static final Item BISMUTH_BOOTS = new ArmorItem(BISMUTH_ARMOR, EquipmentSlot.FEET, new FabricItemSettings().group(Main.GEAR_ITEM_GROUP));
+
+	//Blocks
+	public static final Block BISMUTH_ORE = new BismuthOre(FabricBlockSettings.copyOf(Blocks.IRON_ORE));
+
 
 
 
@@ -43,6 +51,9 @@ public class Main implements ModInitializer {
 		Registry.register(Registry.ITEM, new Identifier("gearmod", "bismuth_chestplate"), BISMUTH_CHESTPLATE);
 		Registry.register(Registry.ITEM, new Identifier("gearmod", "bismuth_leggings"), BISMUTH_LEGGINGS);
 		Registry.register(Registry.ITEM, new Identifier("gearmod", "bismuth_boots"), BISMUTH_BOOTS);
+
+		Registry.register(Registry.BLOCK, new Identifier("gearmod", "bismuth_ore"), BISMUTH_ORE);
+		Registry.register(Registry.ITEM, new Identifier("gearmod", "bismuth_ore"), new BlockItem(BISMUTH_ORE, new FabricItemSettings()));
 
 
 	}
---
+++ src/main/java/net/fabricmc/gear/items/blocks/BismuthOre.java
@@ -0,0 +1,9 @@
+package net.fabricmc.gear.items.blocks;
+
+import net.minecraft.block.Block;
+
+public class BismuthOre extends Block {
+    public BismuthOre(Settings settings) {
+        super(settings);
+    }
+}
